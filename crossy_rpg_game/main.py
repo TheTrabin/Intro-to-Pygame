43e24125fe145020cbbf30a88590d19bf4dc5b23
TheTrabin <beyondtears@gmail.com>
2021-04-30 18:15:56 -0700

    Added basic loop

--- crossy_rpg_game/main.py
+++ crossy_rpg_game/main.py
@@ -9,16 +9,24 @@ width = 800
 height = 800
 white_color = (255, 255, 255)
 
-
 #The Game Window itself
+
 # Game Screen - Takes in a Tuple: width, height in pixels
-game_window = pygame.display.set_mode((width,height))
+game_window = pygame.display.set_mode((width, height))
 
-# Add color to the window
-game_window.fill(white_color)
-#The display update loop
-pygame.display.update()
+clock = pygame.time.Clock() #The Clock, or how often things update
 
+#The loop - This is what makes the game go so it doesn't instantly quit out
+while True:
+    # Handle Events
+
+    # Execute Logic
+
+    # Update Display  
+    game_window.fill(white_color) # Add color to the window
+    pygame.display.update()  # The display update loop
+
+    clock.tick(60) #how often it updates - This is set to 60 times per second.
 
 #When everything is done, make sure to call quit from pygame and then call quit itself
 pygame.quit()
